1c260bb459a0b5822989d057d250fbdf957f4bf0
Fernando Vitale <fpvvitale@gmail.com>
2017-08-31 08:32:58 -0300

    trabajo en progreso - agregando busqueda

--- app/models/profesional.rb
+++ app/models/profesional.rb
@@ -1,4 +1,16 @@
 class Profesional < ApplicationRecord
   has_one :matricula
   has_many :domicilio
+
+  def self.search(termino_buscado, attributes)
+    if termino_buscado && attributes
+      results = []
+      attributes.each do |att|
+        results = results.empty? ? (Profesional.joins(:matricula).joins(:domicilio).where("#{att} LIKE ?", "%#{termino_buscado}%")) : results + Profesional.joins(:matricula).joins(:domicilio).where("#{att} LIKE ?", "%#{termino_buscado}%")
+      end
+      results
+    else
+      all
+    end
+  end
 end
