d35efff59a0376b7b61d99222fda13563ef333ac
juampavilla <juampavilla@gmail.com>
2017-09-22 23:00:26 -0300

    adding session1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root :to => "pages#home"
 
   get '/home', to: 'pages#home'
   get  '/about',   to: 'pages#about'
   get  '/contact', to: 'pages#contact'
   get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   get  'search/index', to: 'search#index'
 
