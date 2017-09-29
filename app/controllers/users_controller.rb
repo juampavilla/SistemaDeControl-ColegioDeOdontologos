59ceeb2dd6cb8765a93895f90d4b5b3ea8675e2a
juampavilla <juampavilla@gmail.com>
2017-09-28 23:50:09 -0300

    finish basicLogin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
-      # redirect_to user_url(@user)
+      log_in @user
       flash[:success] = "¡Bienvenido a SACO!"
       redirect_to @user
     else
