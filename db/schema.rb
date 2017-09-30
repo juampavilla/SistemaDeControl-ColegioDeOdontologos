17e5ab658aaf34b053f7ba1bcd391fd4405e4013
juampavilla <juampavilla@gmail.com>
2017-09-30 20:35:20 -0300

    remember digest, and, fix tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911020536) do
+ActiveRecord::Schema.define(version: 20170930231235) do
 
   create_table "domicilios", force: :cascade do |t|
     t.string "cp"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170911020536) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
