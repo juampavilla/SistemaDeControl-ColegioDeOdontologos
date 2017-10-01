ffb2c8f2ff97e872943c92139bbe8292dd0e8804
juampavilla <juampavilla@gmail.com>
2017-09-30 21:20:32 -0300

    fix problem with two or more browsers

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -23,20 +23,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
-      password: 'password' } }
-      assert is_logged_in?
-      assert_redirected_to profesionales_path
-      follow_redirect!
-      assert_template 'profesionales/index'
-      assert_select "a[href=?]", login_path, count: 0
-      assert_select "a[href=?]", logout_path
-      assert_select "a[href=?]", user_path(@user)
-      delete logout_path
-      assert_not is_logged_in?
-      assert_redirected_to root_url
-      follow_redirect!
-      assert_select "a[href=?]", login_path
-      assert_select "a[href=?]", logout_path,      count: 0
-      assert_select "a[href=?]", user_path(@user), count: 0
-    end
+                                password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to profesionales_path
+    follow_redirect!
+    assert_template 'profesionales/index'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
+end
