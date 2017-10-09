7b80e9fa493d27632faf9ac1463bcd265ec62581
juampavilla <juampavilla@gmail.com>
2017-10-08 21:54:13 -0300

    finishing testing of loging

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -66,6 +66,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  ############authenticated user###################
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
   end
