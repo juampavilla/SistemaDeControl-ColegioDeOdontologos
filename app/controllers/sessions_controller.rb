3cad63e083dc09067b45edbe77f4e50b6f18560c
juampavilla <juampavilla@gmail.com>
2017-10-11 00:40:58 -0300

    fixing forgeting users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == 1 ? remember(user) : forget(user)
-      remember user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       #redirect_to user
       redirect_to profesionales_path
     else
