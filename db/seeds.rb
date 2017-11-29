4890952d6505e064155f1235dbd68ecc65d01a17
juampavilla <juampavilla@gmail.com>
2017-11-28 21:58:37 -0300

    refactoring users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: "example@prueba.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+
+  email = "example-#{n+1}@prueba.org"
+  password = "password"
+  User.create!(email: email,
+               password:              password,
+               password_confirmation: password)
+end
