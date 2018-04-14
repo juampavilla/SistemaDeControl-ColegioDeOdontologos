89ef1e0f3b21608dca324f73061e99f70c539e52
Fernando Vitale <fpvvitale@gmail.com>
2018-04-13 21:33:24 -0300

    simplified search to include all attributes always

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
+    attributes = ['nombres', 'apellido', 'nro_doc', 'tipo_doc', 'matriculas.matricula']
     @result = Profesional.search(search_params[:termino_buscado], attributes)
     respond_to do |format|
       format.html
@@ -8,16 +9,6 @@ class SearchController < ApplicationController
   end
 
   private
-  def attributes
-    attributes = []
-    attributes << 'nombres'   if search_params[:nombre]
-    attributes << 'apellido'  if search_params[:nombre]
-    attributes << 'nro_doc'   if search_params[:documento]
-    attributes << 'tipo_doc'  if search_params[:documento]
-    attributes << 'matriculas.matricula' if search_params[:matricula]
-    attributes
-  end
-
   def search_params
     params.permit(:termino_buscado, :nombre, :documento, :matricula, :format)
   end
