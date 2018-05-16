577521bc2cda3f153e95324200aae7ea66137b0c
juampavilla <juampavilla@gmail.com>
2018-05-16 07:39:02 -0300

    working

--- app/models/pago.rb
+++ app/models/pago.rb
@@ -4,10 +4,18 @@ class Pago < ApplicationRecord
 
   include ReportsKit::Model
   reports_kit do
-    aggregation :monto , :monto_abonado
+    aggregation :sum_pago , [:sum, 'pagos.monto_abonado']
+  #  aggregation :average_time_to_publish, [:average, 'posts.published_at - posts.created_at']
      #contextual_filter :for_profesional, ->(relation, context_params) { relation.where(profesional_id: context_params[:profesional_id]) }
      #contextual_filter :for_profesional, ->(relation, pago_params) { relation.where.not(profesional_id: nil) }
   #  filter :is_published, :boolean, conditions: ->(relation) { relation.where(status: 'published') }contextual_filter :for_author, ->(relation, context_params) { relation.where(author_id: context_params[:author_id]) }
+  end
+
+
+  STATUSES = %w(draft private published).freeze
+
+  def to_s
+    monto_abonado
   end
 
 end
