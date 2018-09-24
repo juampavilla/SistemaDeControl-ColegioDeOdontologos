ce4d099bce31ca55a339a8cb6f407901a8e4dbc6
juampavilla <juampavilla@gmail.com>
2018-09-24 07:38:34 -0300

    delete byebug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'sistemasaco2018@gmail.com'
 
   def welcome_email(user)
-    byebug
     @user = user
     @url  = 'http://sistemasaco2018'
-    mail(to: "juampavilla@gmail.com", subject: 'Bienvenido a SACO' )    
+    mail(to: "juampavilla@gmail.com", subject: 'Bienvenido a SACO' )
   end
 end
