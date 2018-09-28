6c0d2f171cd3d0f0d85cd5d5995066f5463e96ec
juampavilla <juampavilla@gmail.com>
2018-09-27 21:02:15 -0300

    soporte mercado pago

--- Gemfile
+++ Gemfile
@@ -45,7 +45,7 @@ gem 'rubyzip', '= 1.0.0'
 gem 'axlsx', '= 2.0.1'
 gem 'axlsx_rails'
 # mercado pago
-gem 'mercadopago'
+gem 'mercadopago-sdk'
 
 
 # Use Capistrano for deployment
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,21 +59,21 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'sistemasaco2018@gmail.com'}
 
-  config.action_mailer.smtp_settings = {
-    user_name:      ENV['SENDMAIL_USERNAME'],
-    password:       ENV['SENDMAIL_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
   # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   user_name: 'sistemasaco2018@gmail.com',
-  #   password: 'juampa2018',
-  #   authentication: 'plain',
+  #   user_name:      ENV['SENDMAIL_USERNAME'],
+  #   password:       ENV['SENDMAIL_PASSWORD'],
+  #   domain:         ENV['MAIL_HOST'],
+  #   address:       'smtp.gmail.com',
+  #   port:          '587',
+  #   authentication: :plain,
   #   enable_starttls_auto: true
   # }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'sistemasaco2018@gmail.com',
+    password: 'juampa2018',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
